f842e063ad111e3922d68eb47028d1020e8cf06f
marquescleiton <cleiton.contato2210@gmail.com>
2025-08-24 15:03:50 -0300

    voltando commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,2 @@
+// types.d.ts ou vite-env.d.ts
+declare module 'vite-plugin-pwa';
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/pwa-sheets-helloworld/',
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Vafyndell',
+        short_name: 'Vafyndell',
+        start_url: '/pwa-sheets-helloworld/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#0d6efd',
+        icons: [
+          {
+            src: 'Vafyndell-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'Vafyndell-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
     server: {
     port: 3000
