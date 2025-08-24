facc4be09c742587fd36f3fd345a51ba3b7e25d1
MarquesCleiton <MarquesCleiton@users.noreply.github.com>
2025-08-24 13:52:47 +0000

    deploy: 61a4197bf654112d7484685da39ce744b07e41ea

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+// service-worker.js
+
+const CACHE_NAME = "pwa-cadastro-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./dist/main.js"
+];
+
+// Instala e armazena arquivos no cache
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  console.log("Service Worker instalado");
+});
+
+// Ativação do SW
+self.addEventListener("activate", event => {
+  console.log("Service Worker ativado");
+});
+
+// // Intercepta requisições e responde com cache se offline
+// self.addEventListener("fetch", event => {
+//   event.respondWith(
+//     fetch(event.request).catch(() => caches.match(event.request))
+//   );
+// });
