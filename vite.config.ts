fa95da55e2cf38c678cbabee119f95853b526d75
marquescleiton <cleiton.contato2210@gmail.com>
2025-08-24 20:14:32 -0300

    página de cadastro

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     build: {
     rollupOptions: {
       input: {
-        cadastro: 'src/pages/cadastro.html'
+        cadastro: 'public/pages/cadastro.html'
       }
     }
   },
