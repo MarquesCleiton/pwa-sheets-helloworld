7ca121f623caa0e906b2ca0a4b608334bd0fd132
marquescleiton <cleiton.contato2210@gmail.com>
2025-08-24 20:49:35 -0300

    página de cadastro

--- src/presentation/scripts/cadastro.ts
+++ src/presentation/scripts/cadastro.ts
@@ -3,16 +3,19 @@ import { SheetsClient } from "../../infrastructure/google/SheetsClient";
 const client = new SheetsClient();
 const form = document.getElementById("formCadastro") as HTMLFormElement;
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const nome = (document.getElementById("nome") as HTMLInputElement).value;
-  const email = (document.getElementById("email") as HTMLInputElement).value;
-  const obs = (document.getElementById("obs") as HTMLInputElement).value;
-  await client.appendRowByHeader("Cadastro", {
-    Nome: nome,
-    Email: email,
-    Observacoes: obs
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("formCadastro") as HTMLFormElement;
+  form?.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const nome = (document.getElementById("nome") as HTMLInputElement).value;
+    const email = (document.getElementById("email") as HTMLInputElement).value;
+    const obs = (document.getElementById("obs") as HTMLInputElement).value;
+    await client.appendRowByHeader("Cadastro", {
+      Nome: nome,
+      Email: email,
+      Observacoes: obs
+    });
+    alert("Cadastro realizado com sucesso!");
   });
-  alert("Cadastro realizado com sucesso!");
 });
+
